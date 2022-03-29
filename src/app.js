a059b807b390afc65f19e2c006c6584016fd68af
moralinda <99352472+moralinda@users.noreply.github.com>
2022-03-29 14:36:44 +0100

    Added api call and showed temperature in APP

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
+function showTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  document.querySelector("#temperature").innerHTML = `${temperature}`;
+}
 
+let apiKey = "46282d6c2bfaf109c807f0208a634585";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=paris&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(showTemperature);
