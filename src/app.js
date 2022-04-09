9a98d774990f5f4cab91f24c2e0078183691952b
moralinda <99352472+moralinda@users.noreply.github.com>
2022-04-09 12:59:10 +0100

    Showing current Temperature and weather conditions

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 function showTemperature(response) {
-  let temperature = Math.round(response.data.main.temp);
-  document.querySelector("#temperature").innerHTML = `${temperature}`;
+  let currentTemperature = Math.round(response.data.main.temp);
+  document.querySelector("#temperature").innerHTML = `${currentTemperature}`;
+  let currentCity = response.data.name;
+  document.querySelector("#city").innerHTML = `${currentCity}`;
+  let currentDescription = response.data.weather[0].description;
+  document.querySelector("#description").innerHTML = `${currentDescription}`;
+  let currentHumidity = response.data.main.humidity;
+  document.querySelector("#humidity").innerHTML = `${currentHumidity}`;
+  let currentWind = Math.round(response.data.wind.speed);
+  document.querySelector("#wind").innerHTML = `${currentWind}`;
 }
 
 let apiKey = "46282d6c2bfaf109c807f0208a634585";
